aff840596b60da879fc99f0955cdb6f317412e82
superkid <8140299+hahakid@users.noreply.github.com>
2019-04-12 14:10:28 +0800

    the script

---
+++ data/process_video/extract_video.sh
@@ -0,0 +1,2 @@
+#./yolo_mark /home/kid/dataset/trafficlight/615/hui/ cap_video /home/kid/dataset/trafficlight/615/hui.avi 15
+./yolo_mark /home/kid/dataset/trafficlight/615/qu/ cap_video /home/kid/dataset/trafficlight/615/qu.avi 15
